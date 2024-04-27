592a3de25bc29ba4f294f1fb37d67c08acdf308c
Marc Mel L. Cabiling <162778303+Verus2311@users.noreply.github.com>
2024-04-27 08:56:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let loginForm = document.getElementById("Registration Form");
+// SELECTING STUDENT
+console.log("Selecting Student");
+let selectedStudent = ["New Student", "Old Student"];
+console.log(selectedStudent[1]);
+// STUDENT'S NAME
+console.log("Student's Full Name");
+let fullName = {
+ firstName: "Marc Mel",
+ middleName: "Lerpido",
+ lastName: "Cabiling"
+};
+console.log(fullName);
+// AGE
+console.log("Student's Age");
+let Age = "20";
+console.log(Age); 
+// INFORMATION
+console.log("Student's Information");
+let information = {
+    email: "marclerpido23@gmail.com"
+   };
+console.log(information);
+// USERNAME & PASSWORD
+let myUsername = document.getElementById('userName');
+console.log(myUsername);
